e78da35ac2b106865e19d467f50bc34900d9d72a
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2026-02-05 17:37:50 +0530

    test(api): add application context test

---
+++ okta-rbac-api/src/test/java/com/example/okta_rbac_api/OktaRbacApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okta_rbac_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaRbacApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
